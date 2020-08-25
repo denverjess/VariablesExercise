4582ae80153eedc935003e22af2584bfe1491b81
Your Namedenverjess <jessarbar@gmail.com>
2020-08-25 11:39:58 -0600

    Add project files.

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+       static void Main(string[] args)
+			{
+				string sonName = "Odin";
+				int sonAge = 6;
+				double realAge = 6.5;
+				char eyes = ':';
+				char smile = ')';
+
+				Console.WriteLine($"My son, {sonName}, is {sonAge} years old, though he would say he's {realAge}. {eyes}{smile}");
+
+        }
+    }
+}
